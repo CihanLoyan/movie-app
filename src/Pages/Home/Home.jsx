d1bc3b0836631609657d81805631d970440cfd4c
Cihan Loyan <cihanloyan@gmail.com>
2024-05-17 03:49:34 +0300

    lazy loading

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,19 @@
-import React, {useEffect, useState } from 'react'
+import React, {lazy, useEffect, useState } from 'react'
 import Genres from '../../components/genres/genres'
-import MoveList from '../../components/movieList/movieList'
+// import MovieList from '../../components/movieList/movieList'
+const MovieList = lazy(() => import('../../components/movieList/movieList'))
 
 import { useDispatch, useSelector } from 'react-redux'
+import { Suspense } from 'react'
 
 import { getGenres } from '../../redux/slices/genreSlice'
 import { getMovieList } from '../../redux/slices/movieListSlice'
 import { getMovieListByGenre } from '../../redux/slices/movieListSlice'
 
+
 const Home = () => {
+    // const [loading, setLoading] = useState(false)
+
     const dispatch = useDispatch()  
     const { genres } = useSelector((store) => store.genre)  // genres: state[]
     // dispatch(getGenres()) çalıştığında genres state'i güncellenir.
@@ -19,11 +24,19 @@ const Home = () => {
     // Seçili kategori id'sini tutan state
 
     useEffect(() => {
-      dispatch(getMovieList())
+      /* setTimeout(()=> {
+        console.log("...50 ms");
+        setLoading(true)  // 500 ms sonra loading true olunca Skeleton'dan çıktı.
+    }, 500) */
+      dispatch(getMovieList())  // 500 ms sonra loading true olduğunda getMovieList() çalıştı.
       console.log("getMovieList()");
     }, [])  // Component ilk ve her render edildiğinde çalışır. 
     
     useEffect(() => {
+      /* setTimeout(()=> {
+        console.log("...500 ms");
+        setLoading(true)  // 500 mn sonra loading true olunca Skeleton'dan çıktı.
+    }, 500) */
       dispatch(getMovieListByGenre(selectedGenreState))
       console.log("selectedGenreState:" + selectedGenreState);
     }, [selectedGenreState])  // seçili tür id'sini tutan state her değiştiğinde çalışır.
@@ -38,8 +51,10 @@ const Home = () => {
 
   return (
     <div>
-      <Genres genres={genres} onSelectedGenre={selectedGenre}/>
-      <MoveList movieList={movieList} selectedGenreState={selectedGenreState}/>
+        <Genres genres={genres} onSelectedGenre={selectedGenre}/>
+        <Suspense fallback={<>...Loading Movie List...</>}>  
+            <MovieList movieList={movieList} selectedGenreState={selectedGenreState}/>
+        </Suspense>
     </div>
   )
 }
