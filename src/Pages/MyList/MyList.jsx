3355296e6e9d920fc1f1a7c8f189aa188fd90a6e
Cihan Loyan <cihanloyan@gmail.com>
2024-05-19 04:51:46 +0300

    Added pagination

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from 'react-redux'
 import MovieCard from '../../components/movieCard/movieCard'
 import './MyList.css'
+import ReactPaginate from 'react-paginate';
+import { GrPrevious, GrNext } from "react-icons/gr";
 
 const MyList = () => {
 
   const { movies } = useSelector((store) => store.favorite)
-
   console.log(movies);
 
+
+  const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 8; 
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = movies.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(movies.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % movies.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    }
   return (
     <div className='my-list'>
         <h1>Your Favorite Movies</h1>
         <ul>
             {
-              movies && movies.map((movie, index) => (
+              currentItems && currentItems.map((movie, index) => (
                   <MovieCard key={movie.id + index} movie={movie} />
               ))
             }
         </ul>
+        <div className='pagination-component'>
+                <ReactPaginate
+                    className='pagination'
+                    breakLabel="..."
+                    nextLabel={<GrNext />}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel={<GrPrevious />}
+                    renderOnZeroPageCount={null}
+                />
+            </div>
     </div>
   )
 }
--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState} from 'react'
 import MovieCard from '../movieCard/movieCard'
 import '../movieList/movieList.css'
+import ReactPaginate from 'react-paginate';
+import { GrPrevious, GrNext } from "react-icons/gr";
 
 
 const MovieList = ({ selectedGenreState, movieList, genres }) => {
@@ -15,15 +17,42 @@ const MovieList = ({ selectedGenreState, movieList, genres }) => {
         setGenreName(getGenreName())
     }, [selectedGenreState])
     
+
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 8; 
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = movieList.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(movieList.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % movieList.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    }
+
     return (
         <div className='movie-list'>
             <h1>{genreName}</h1>
             <ul>
-              {movieList && movieList.map((movie, index) => (
+              {currentItems && currentItems.map((movie, index) => (
                 <MovieCard key={movie.id + index} movie={movie} />
               ))}
             </ul>
-            
+            <div className='pagination-component'>
+                <ReactPaginate
+                    className='pagination'
+                    breakLabel="..."
+                    nextLabel={<GrNext />}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel={<GrPrevious />}
+                    renderOnZeroPageCount={null}
+                />
+            </div>
         </div>
     )
 }
