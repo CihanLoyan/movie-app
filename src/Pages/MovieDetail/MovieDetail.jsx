61634af84fc50aba01f4c45a0589ae24d785a3ac
Cihan Loyan <cihanloyan@gmail.com>
2024-05-19 13:12:10 +0300

    Fix errors

--- src/Pages/MovieDetail/MovieDetail.jsx
+++ src/Pages/MovieDetail/MovieDetail.jsx
@@ -17,7 +17,8 @@ const MovieDetail = () => {
     console.log(id);
 
     const { movieDetail } = useSelector((store) => store.movieDetail)
-    const { title, vote_average, overview, genres, poster_path, spoken_languages, release_date} = movieDetail
+    // const { title, vote_average, overview, genres, poster_path, spoken_languages, release_date} = movieDetail
+    const {title, vote_average, poster_path } = movieDetail;
     const dispatch = useDispatch();
 
     const { movies } = useSelector((store) => store.favorite)
@@ -27,7 +28,7 @@ const MovieDetail = () => {
 
     const [isFavorite, setIsFavorite] = useState(false)  // İlgili filmin favorilere ekli olup olmadığı bilgisini tutan state.
     useEffect(() => {
-        const isFavorite = movies && movies.find((movie) => movie.id == id)  // Eğer ilgili film localStorage'da ise değişkene aktar ve
+        const isFavorite = movies?.find((movie) => movie.id == id)  // Eğer ilgili film localStorage'da ise değişkene aktar ve
         if(isFavorite) {  // Bu değişken var ise
             setIsFavorite(true)  // Favorilerde olduğunu belirtiyoruz.
         } else {
@@ -41,7 +42,7 @@ const MovieDetail = () => {
       console.log("getMovieById()");
     }, [id])
 
-    const release_year = new Date(release_date)  // Sadece yıl bilgisini çekmek için.
+    const release_year = new Date(movieDetail?.release_date)  // Sadece yıl bilgisini çekmek için.
 
     const addFavorite = () => {
         const payload = {
@@ -63,7 +64,7 @@ const MovieDetail = () => {
   return (
       <div className="movie-detail">
           <header>
-            <p>{title}</p>
+            <p>{movieDetail?.title}</p>
             <div className='add-favorite-remove'>
                {
                 isFavorite ?    // yukarıdan true dönerse ilgili film zaten favorilerde olduğu için remove movie butonu gösterilir..
@@ -82,35 +83,35 @@ const MovieDetail = () => {
           <div className='content'>
             <div className='left'>
                 <div className='movie-backdrop_path'>
-                    <img src={`${API_IMG}/${poster_path}`} alt={title} />
+                    <img src={`${API_IMG}/${movieDetail?.poster_path}`} alt={movieDetail.title} />
                 </div>
             </div>
             <div className='right'>
                 <div className='movie-overview'>
-                    <p>{overview}</p>
+                    <p>{movieDetail?.overview}</p>
                 </div>
                 <div className="movie-rating">
                     <FaStar />
-                    <p>{vote_average.toFixed(1)}</p>
+                    <p>{movieDetail?.vote_average?.toFixed(1)}</p>
                 </div>
                 <div className='release-date'>
                     <span>Year:</span>
-                    <p>{release_year.getFullYear()}</p>
+                    <p>{release_year?.getFullYear()}</p>
                 </div>
                 <div className='movie-info'>
                     <div className='movie-genres'>
                         <span>Genre:</span>
                         <ul>
-                            {genres && genres.map((genre) => (
-                              <li key={genre.id} className="movie-genre">{genre.name}</li>
+                            {movieDetail.genres && movieDetail.genres.map((genre) => (
+                              <li key={genre?.id} className="movie-genre">{genre?.name}</li>
                             ))}
                         </ul>
                     </div>
                     <div className='movie-languages'>
                         <span>Language:</span>
                         <ul>
-                            {spoken_languages && spoken_languages.map((language, index) => (
-                                <li key={index}>{language.english_name}</li>
+                            {movieDetail?.spoken_languages && movieDetail?.spoken_languages.map((language, index) => (
+                                <li key={index}>{language?.english_name}</li>
                             ))}
                         </ul>
                     </div>
